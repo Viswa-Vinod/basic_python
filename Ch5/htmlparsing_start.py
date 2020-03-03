307eec6d17d23acb38939d3f00583ed586933fb8
Vinod Kumar <vinod@gethyphen.com>
2020-03-03 18:59:26 +0530

    html parsing

--- Ch5/htmlparsing_start.py
+++ Ch5/htmlparsing_start.py
@@ -1,13 +1,47 @@
 # 
 # Example file for parsing and processing HTML
 #
+from html.parser import HTMLParser
 
+metaCount = 0
+class MyHTMLParser(HTMLParser):
+  def handle_comment(self, data):
+    print("encountered comment: ", data)
+    pos = self.getpos()
+    print("\tA line: ", pos[0], " position ", pos[1])
+  
+  def handle_starttag(self, tag, attrs):
+    global metaCount
+    if tag == "meta":
+      metaCount += 1
+    print("encountered start tag: ", tag)
+    pos = self.getpos()
+    print("\tAt line: ", pos[0], " position ", pos[1])
+
+    if attrs.__len__() > 0:
+      print("\tAttributes")
+      for a in attrs:
+        print("\t", a[0], "=", a[1])
+
+  def handle_endtag(self, tag):
+    print("encountered end tag: ", tag)
+    pos = self.getpos()
+    print("\tAt line: ", pos[0], " position ", pos[1])
+
+  def handle_data(self, data):
+    if data.isspace():
+      return
+    print("encountered data: ", data)
+    pos = self.getpos()
+    print("\tAt line: ", pos[0], " position ", pos[1])
 
 def main():
   # instantiate the parser and feed it some HTML
   parser = MyHTMLParser()
-    
-
+  f = open("./Ch5/samplehtml.html")
+  if f.mode == 'r':
+    contents = f.read()
+    parser.feed(contents)
+  print("Meta tags found: ", str(metaCount))
 if __name__ == "__main__":
   main();
-  
